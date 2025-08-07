a2169fd06e352c35ecd948ebe0b19419eff9e017
tgraha09 <tfire09@gmail.com>
2025-08-07 01:21:39 -0500

    Added idle and walking animation functionality with transitions

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,29 +1,48 @@
 extends CharacterBody3D
-#extends PlayerMovement #"res://Scripts/Player/player_movement.gd"
-@onready var anim_player: AnimationPlayer = $visuals/Player_model/AnimationPlayer
-#var player_movement: PlayerMovement
-#var anim_player = player_movement.animation_player
-#@onready var player_model: Node3D = $visuals/Player_model
-var animtation = MovementLogic
 
+@onready var anim_player: AnimationPlayer = $visuals/Player_base/AnimationPlayer
+@onready var anim_tree: AnimationTree = $visuals/Player_base/AnimationTree
 
-var first_time = false
+@export var blend_speed := 5.0
+@export var speed := 5.0
 
+var player_logic = MovementLogic
+var first_time := false
 
 func _ready():
-	if first_time==false:
+	if not first_time:
 		print("Started Player Process")
-		animtation.set("animation_player", anim_player)
-		animtation._play("Idle", true)
+		# Initialize references FIRST
+		player_logic.animation_player = anim_player
+		player_logic.animation_tree = anim_tree
+
+		# Activate AnimationTree BEFORE setting parameters
+		anim_tree.active = true
+
+		# Force initial state (order matters!)
+		player_logic.change_state(player_logic.States.IDLE)
+		#anim_tree.set("parameters/idle_blend_walk/Walking/blend_amount", 0.0)
+		
+		# Debug: Print all parameters to verify paths
+		
 		first_time = true
-	
 
-
-#func _process(delta: float) -> void:
-	#Movement._playAnimation("Idle", true)
-	#PlayerMovement._playAnimation("Idle", true)
-	#PlayerMovement._playAnimation("Idle", true)
-		#player_movement._playAnimation("Idle", true)
-	#if first_time==true:
-		#animation_player.play("Idle")
+func _physics_process(delta: float):
+	# Get 3D movement input
+	var input_dir = Vector3(
+		Input.get_axis("left", "right"),  # X axis (left/right)
+		0,                                # Y axis (up/down - unused for ground movement)
+		Input.get_axis("forward", "backward")  # Z axis (forward/backward)
+	)
 	
+	# Normalize and apply speed
+	if input_dir.length() > 0:
+		velocity = input_dir.normalized() * speed
+	else:
+		velocity = Vector3.ZERO
+	
+	# Update movement logic
+	player_logic.velocity = velocity.length()
+	player_logic.handle_states(delta, blend_speed)
+	
+	move_and_slide()
--- Scripts/Player/player_movement.gd
+++ Scripts/Player/player_movement.gd
@@ -1,18 +1,86 @@
-#extends CharacterBody3D
+class_name PlayerMovement
 extends Node
 
-class_name PlayerMovement
+# Proper type hints
+var animation_player: AnimationPlayer
+var animation_tree: AnimationTree
 
-var animation_player: AnimationPlayer = null
-#var animation_player: AnimationPlayer
+enum States {IDLE, RUN, JUMP, WALK, NONE}
+var currentState = States.NONE
 
-func _play(_name: String, val: bool):
-	if animation_player == null or not animation_player.has_animation(_name):
-		return
-	if val:
-		animation_player.play(_name)
-	else:
-		animation_player.stop()
+# Movement parameters
+var velocity := 0.0
+var max_walk_speed := 5.0
+var blend_speed := 1.0
+var delta := 0.0
+var speed = 0
+const condition_path = "parameters/conditions/"
+func change_state(newState):
+	currentState = newState
 
-func _ready():
-	print("Player Movement Ready")
+func handle_states(_delta, _blend_speed):
+	delta = _delta
+	blend_speed = _blend_speed
+	
+	match currentState:
+		States.IDLE:
+			_idle()
+		States.WALK:
+			_walk()
+		States.RUN:
+			_run()
+
+func _idle():
+	#print("IDLE")
+	var current_blend = animation_tree.get("parameters/idle_blend_walk/Walking/blend_amount")
+	var new_blend = lerpf(current_blend, 0.0, delta * blend_speed*2.0)
+	animation_tree.set("parameters/idle_blend_walk/Walking/blend_amount", new_blend)
+	#print("current_blend IDLE: ", current_blend)
+
+	if Input.is_action_pressed("forward"):
+		change_state(States.WALK)
+		animation_tree["parameters/playback"].travel("Walking")
+		#animation_tree.set("parameters/conditions/idle_to_walk", true)
+	
+
+
+func _walk():
+	print("WALK")
+	# Only update blend if in transition state
+	var current_blend = animation_tree.get("parameters/idle_blend_walk/Walking/blend_amount")
+	var new_blend = lerpf(current_blend, 1.0, delta * blend_speed)
+	animation_tree.set("parameters/idle_blend_walk/Walking/blend_amount", new_blend)
+	
+	if Input.is_action_just_released("forward"):
+		change_state(States.IDLE)
+		#animation_tree.set("parameters/conditions/walk_to_idle", true)
+		animation_tree["parameters/playback"].travel("Idle")
+
+func _run():
+	# Similar to walk but with different blend parameters
+	if Input.is_action_just_released("shift"):
+		change_state(States.WALK)
+	elif not Input.is_anything_pressed():
+		change_state(States.IDLE)
+		
+func trigger_animations(_conditions: Dictionary):
+	print("trigger_animations")
+	for parameter in animation_tree.get_property_list():
+		#print(parameter.name)
+		if parameter.name.begins_with(condition_path) && parameter.type == TYPE_BOOL:
+			#print(parameter.name)
+			for condition in _conditions:
+				#print(condition)
+				if parameter.name.contains(condition):
+					var dict_value = _conditions[condition]
+					#print(condition)
+					#print(dict_value)
+					animation_tree.set(parameter.name, dict_value)
+				else:
+					animation_tree.set(parameter.name, false)
+			#		animation_tree.set(parameter.name, true)
+					#print(parameter.name)
+					#print(true)
+				#else:
+					#animation_tree.set(parameter.name, false)
+					#print(false)
